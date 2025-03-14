6fbd583c405f3d1a28a23ab047e98ed240cba9c1
Samuel Deolino <samueldeolino@alu.ufc.br>
2025-03-14 09:23:31 -0300

    Project created

---
+++ src/main/java/com/samueldev/course_springboot/CourseSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.samueldev.course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samueldev/course_springboot/CourseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueldev.course_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
