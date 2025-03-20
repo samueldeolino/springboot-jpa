60410e3ecaa7e8736e07f10311760a2cb6aa550f
Samuel Deolino <samueldeolino@alu.ufc.br>
2025-03-20 10:05:10 -0300

    service layer, component registration

--- src/main/java/com/samueldev/course_springboot/resources/UserResource.java
+++ src/main/java/com/samueldev/course_springboot/resources/UserResource.java
@@ -1,20 +1,28 @@
 package com.samueldev.course_springboot.resources;
 import com.samueldev.course_springboot.entities.User;
+import com.samueldev.course_springboot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(Long id) {
-        User user = new User(1L, "Samuel", "Samuelgmail.com");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 
-//    @PostMapping
-//    public ResponseEntity<User> saveUser(@RequestBody User user) {
-//        return
-//    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+         User object = service.findById(id);
+         return ResponseEntity.ok().body(object);
+    }
 }
---
+++ src/main/java/com/samueldev/course_springboot/services/UserService.java
@@ -0,0 +1,25 @@
+package com.samueldev.course_springboot.services;
+
+import com.samueldev.course_springboot.entities.User;
+import com.samueldev.course_springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> object = repository.findById(id);
+        return object.get();
+    }
+}
