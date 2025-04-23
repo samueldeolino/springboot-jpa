6f20d0fc44e5696d424f4acaeddc4ce795fd5ac5
Samuel Deolino <samueldeolino@alu.ufc.br>
2025-04-23 14:36:04 -0300

    user insert

--- src/main/java/com/samueldev/course_springboot/resources/UserResource.java
+++ src/main/java/com/samueldev/course_springboot/resources/UserResource.java
@@ -1,10 +1,14 @@
 package com.samueldev.course_springboot.resources;
+
 import com.samueldev.course_springboot.entities.User;
+import com.samueldev.course_springboot.services.CategoryService;
 import com.samueldev.course_springboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,6 +17,8 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
@@ -24,5 +30,13 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
          User object = service.findById(id);
          return ResponseEntity.ok().body(object);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody User user){
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+                path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
